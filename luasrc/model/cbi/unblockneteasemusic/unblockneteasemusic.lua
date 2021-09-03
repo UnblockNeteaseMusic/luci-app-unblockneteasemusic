6289fec42b86cdbc04867b01389a2b3ba030341e
Tianling Shen <cnsztl@immortalwrt.org>
2021-09-04 05:17:00 +0800

    chore: update sources
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- luasrc/model/cbi/unblockneteasemusic/unblockneteasemusic.lua
+++ luasrc/model/cbi/unblockneteasemusic/unblockneteasemusic.lua
@@ -14,15 +14,15 @@ enable.rmempty = false
 
 music_source = s:option(Value, "music_source", translate("音源接口"))
 music_source:value("default", translate("默认"))
-music_source:value("netease", translate("网易云音乐"))
-music_source:value("qq", translate("QQ音乐"))
+music_source:value("bilibili", translate("Bilibili音乐"))
+music_source:value("joox", translate("JOOX音乐"))
+music_source:value("kugou", translate("酷狗音乐"))
 music_source:value("kuwo", translate("酷我音乐"))
 music_source:value("migu", translate("咪咕音乐"))
-music_source:value("kugou", translate("酷狗音乐"))
-music_source:value("baidu", translate("百度音乐"))
-music_source:value("joox", translate("JOOX音乐"))
+music_source:value("pyncmd", translate("网易云音乐（pyncmd）"))
+music_source:value("qq", translate("QQ音乐"))
 music_source:value("youtube", translate("Youtube音乐"))
-music_source:value("bilibili", translate("Bilibili音乐"))
+music_source:value("ytdownload", translate("Youtube音乐（ytdownload）"))
 music_source.description = translate("自定义模式下，多个音源请用空格隔开")
 music_source.default = "default"
 music_source.rmempty = false

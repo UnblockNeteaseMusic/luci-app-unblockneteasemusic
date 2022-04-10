d6056c65d8fb400adf11c0ef199e59fcc4210c27
Tianling Shen <cnsztl@immortalwrt.org>
2022-04-10 21:14:42 +0800

    fix: use `ps -w` to list processes
    without `-w` it may cause no output in some cases.
    
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- root/usr/share/unblockneteasemusic/debugging.sh
+++ root/usr/share/unblockneteasemusic/debugging.sh
@@ -59,7 +59,7 @@ echo -e "\n"
 echo -e "Running info:"
 procd_running_status="$(/etc/init.d/unblockneteasemusic status)"
 echo -e "PROCD running status: $procd_running_status"
-[ "$procd_running_status" = "running" ] && { ps | grep "unblockneteasemusic" | grep "app\.js" || echo -e "Thread is not found."; }
+[ "$procd_running_status" = "running" ] && { ps -w | grep "unblockneteasemusic" | grep "app\.js" || echo -e "Thread is not found."; }
 echo -e "\n"
 
 [ "$procd_running_status" != "running" ] || {

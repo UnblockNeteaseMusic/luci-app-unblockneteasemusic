ed90a2841d09fe7c7412dc98d5e59bc1f7fb2957
Tianling Shen <cnsztl@immortalwrt.org>
2022-07-30 22:20:02 +0800

    CI: tweak for js branch
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>
    (cherry picked from commit f125b3c1ff302ced39b0d1d815f259dd291698c1)

--- .github/build-ipk.sh
+++ .github/build-ipk.sh
@@ -22,9 +22,9 @@ fi
 TEMP_DIR="$(mktemp -d -p $BASE_DIR)"
 TEMP_PKG_DIR="$TEMP_DIR/$PKG_NAME"
 mkdir -p "$TEMP_PKG_DIR/CONTROL/"
-mkdir -p "$TEMP_PKG_DIR/usr/lib/lua/luci/"
+mkdir -p "$TEMP_PKG_DIR/www/"
 
-cp -fpR "$PKG_DIR/luasrc"/* "$TEMP_PKG_DIR/usr/lib/lua/luci/"
+cp -fpR "$PKG_DIR/htdocs"/* "$TEMP_PKG_DIR/www/"
 cp -fpR "$PKG_DIR/root"/* "$TEMP_PKG_DIR/"
 
 echo -e "/etc/config/unblockneteasemusic" > "$TEMP_PKG_DIR/CONTROL/conffiles"
@@ -68,5 +68,5 @@ curl -fsSL "https://raw.githubusercontent.com/openwrt/openwrt/master/scripts/ipk
 chmod 0755 "$TEMP_DIR/ipkg-build"
 "$TEMP_DIR/ipkg-build" -m "" "$TEMP_PKG_DIR" "$TEMP_DIR"
 
-mv "$TEMP_DIR/${PKG_NAME}_${PKG_VERSION}_all.ipk" "$BASE_DIR"
+mv "$TEMP_DIR/${PKG_NAME}_${PKG_VERSION}_all.ipk" "$BASE_DIR/${PKG_NAME}_${PKG_VERSION}_javascript_all.ipk"
 rm -rf "$TEMP_DIR"
